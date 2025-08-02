027eb18c539110b95334ad3af74febdf56ffc7fa
Sandhya-jam <jamsandhyarani@gmail.com>
2025-08-02 14:48:08 +0530

    done building admin Dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -31,6 +31,7 @@ import {PayPalScriptProvider} from '@paypal/react-paypal-js'
 import Order from './Pages/Orders/Order.jsx'
 import UserOrder from './Pages/User/UserOrder.jsx'
 import OrderList from './Pages/Admin/OrderList.jsx'
+import AdminDashboard from './Pages/Admin/AdminDashboard.jsx'
 
 const router=createBrowserRouter(
   createRoutesFromElements(
@@ -58,6 +59,7 @@ const router=createBrowserRouter(
       <Route path='allproducts' element={<AllProducts/>}/>
       <Route path='orderlist' element={<OrderList/>}/>
       <Route path='product/update/:_id' element={<ProductUpdate/>}/>
+      <Route path='dashboard' element={<AdminDashboard/>}/>
     </Route>
     </Route>
   )
